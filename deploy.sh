734c4b336e72be9a4b9c9d58c13a620620ac637c
Aayush-B <aayushbhaglal@gmail.com>
2020-06-11 19:36:03 +0530

    add code to clean up scripts from server post deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 echo "Deploying to server"
 cd ./Resume-Website
 cp -r ./* /var/www/aayushb.com
+rm /var/www/aayushb.com/deploy.sh
+rm /var/www/aayushb.com/clean-up.sh
+rm /var/www/aayushb.com/Jenkinsfile
